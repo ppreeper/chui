beb5541d8304383b1f4409c8c5009e7bb7b89d71
Peter Preeper <ppreeper@gmail.com>
2025-04-15 12:34:53 -0600

    initial commit

---
+++ styles.go
@@ -0,0 +1,42 @@
+package chui
+
+import "github.com/charmbracelet/lipgloss"
+
+// ========= Banner =========
+// func cText(color, msg string) string {
+// 	return color + msg + "{{ .AnsiColor.Default }}"
+// }
+
+// ========= LipGloss =========
+
+var HeaderStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#EEEEEE")).
+	Bold(true)
+
+var EvenRowStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#777777"))
+
+var OddRowStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#AAAAAA"))
+
+var StepStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#EEEEEE")).
+	Bold(true)
+
+var SubStepStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#228822")).
+	Bold(true)
+
+var Success = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#228822"))
+
+var Info = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#EEEEEE"))
+
+var WarningStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#FF8800")).
+	Bold(true)
+
+var ErrorStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#EE0000")).
+	Bold(true)
